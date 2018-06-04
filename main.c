606612a1bef846aeff891dc14a1842cf22d7030d
Christina Wen <c22wen@uwaterloo.ca>
2018-06-04 19:07:40 -0400

    added main file

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  TicTacToe
+//
+//  Created by Christina Wen on 2018-06-04.
+//
+
+// Hi there. Welcome to my game! Just follow the instructions outputed
+//   to play a fun game of 2-player Tic Tac Toe. Good luck!
+
+#include <stdio.h>
+#include "tictactoe.h"
+
+int main(void){
+    play_game();
+    return 0;
+}
